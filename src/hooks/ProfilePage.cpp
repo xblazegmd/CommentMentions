62215a3ef30ff6475195c733f218b8e5b2456d59
xblazegmd <santiagopalaciocolina7@gmail.com>
2025-12-08 17:21:21 -0500

    idrk

--- src/hooks/ProfilePage.cpp
+++ src/hooks/ProfilePage.cpp
@@ -8,8 +8,30 @@ class $modify(CMentionsProfilePage, ProfilePage) {
         if (!ProfilePage::init(accountID, ownProfile)) return false;
         auto btmMenu = ProfilePage::m_mainLayer->getChildByID("bottom-menu");
         if (!btmMenu) {
-            log::error("Could not find bottom-menu (returned null-ptr)");
+            log::error("Could not find bottom-menu (returned nullptr)");
             return true;
+        }
+
+        // auto mentionsBt = CCMenuItemSpriteExtra::create(
+        //     CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+        //     this,
+        //     menu_selector(CMentionsProfilePage::onMentionsBt)
+        // );
+        // btmMenu->addChild(mentionsBt);
+        // mentionsBt->setID("mentions-bt"_spr);
+
+        btmMenu->updateLayout();
+        return true;
+    }
+
+    void getUserInfoFinished(GJUserScore* score) {
+        ProfilePage::getUserInfoFinished(score);
+
+        // Can I set up the button stuff in here maybe?
+        auto btmMenu = m_mainLayer->getChildByID("bottom-menu");
+        if (!btmMenu) {
+            log::error("Could not find bottom-menu (returned nullptr)");
+            return;
         }
 
         auto mentionsBt = CCMenuItemSpriteExtra::create(
@@ -17,11 +39,11 @@ class $modify(CMentionsProfilePage, ProfilePage) {
             this,
             menu_selector(CMentionsProfilePage::onMentionsBt)
         );
+
         btmMenu->addChild(mentionsBt);
         mentionsBt->setID("mentions-bt"_spr);
 
         btmMenu->updateLayout();
-        return true;
     }
 
     void onMentionsBt(CCObject* sender) {
