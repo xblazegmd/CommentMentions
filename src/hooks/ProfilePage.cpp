5710482f7d53a4b330cdf4b6c2c72eb81e37fea0
xblazegmd <santiagopalaciocolina7@gmail.com>
2025-12-08 16:38:31 -0500

    maybe this... idk

--- src/hooks/ProfilePage.cpp
+++ src/hooks/ProfilePage.cpp
@@ -6,7 +6,7 @@ using namespace geode::prelude;
 class $modify(CMentionsProfilePage, ProfilePage) {
     bool init(int accountID, bool ownProfile) {
         if (!ProfilePage::init(accountID, ownProfile)) return false;
-        auto btmMenu = m_mainLayer->getChildByID("bottom-menu");
+        auto btmMenu = ProfilePage::m_mainLayer->getChildByID("bottom-menu");
         if (!btmMenu) {
             log::error("Could not find bottom-menu (returned null-ptr)");
             return true;
