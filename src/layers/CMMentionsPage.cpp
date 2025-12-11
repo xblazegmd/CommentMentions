b8b0ed4b0c7e84418a03a8800e8b3e028d3300a4
xblazegmd <santiagopalaciocolina7@gmail.com>
2025-12-11 11:26:32 -0500

    bg test

--- src/layers/CMMentionsPage.cpp
+++ src/layers/CMMentionsPage.cpp
@@ -1,22 +1,30 @@
 #include "CMMentionsPage.hpp"
+#include "Geode/cocos/platform/CCPlatformMacros.h"
 
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
 namespace layers {
-    bool CMMentionsPage::init() {
-        if (!CCLayer::init()) return false;
-        return true;
+    CMMentionsPage* CMMentionsPage::create() {
+        auto ret = new CMMentionsPage();
+        if (ret && ret->init()) {
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
     }
 
-    CMMentionsPage* CMMentionsPage::create() {
-        auto cmmp = new CMMentionsPage();
-        if (cmmp && cmmp->init()) {
-            cmmp->autorelease();
-            return cmmp;
-        }
-        CC_SAFE_DELETE(cmmp);
-        return nullptr;
+    bool CMMentionsPage::init() {
+        if (!CCLayer::init()) return false;
+
+        auto bg = CCScale9Sprite::create("square_02_001.png");
+        bg->setContentSize({365.f, 40.f});
+        bg->setOpacity(100);
+        this->addChild(bg);
+        
+        this->scheduleUpdate();
+        return true;
     }
 }
