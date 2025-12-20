80e9425df7d83547309d6883758ea5c32e6d3deb
xblazegmd <santiagopalaciocolina7@gmail.com>
2025-12-20 13:51:05 -0500

    removed old queue logic

--- src/core/comments/comments.cpp
+++ src/core/comments/comments.cpp
@@ -4,7 +4,6 @@
 #include <core/formatReq/formatReq.hpp>
 #include <core/history/history.hpp>
 #include <core/notifier/notifier.hpp>
-#include <core/queue/queue.hpp>
 
 #include <Geode/Geode.hpp>
 #include <Geode/utils/base64.hpp>
@@ -39,25 +38,6 @@ namespace comments {
 
     ListenerTask CommentListener::startListener() {
         while (true) {
-            // auto q = queue::loadQueue();
-            // if (!q.empty() && !PlayLayer::get()) { // '!PlayLayer::get() since we don't wanna pop the queue while the user is playing, duh
-            //     log::info("Popping queue...");
-            //     queue::clearQueue();
-
-            //     for (const auto& mention : q) {
-            //         auto qUserIt = mention.find("authorUsername");
-            //         auto qMsgIt = mention.find("comment");
-
-            //         // Fallbacks
-            //         std::string qUser = qUserIt != mention.end() ? qUserIt->second : "Someone";
-            //         std::string qMsg = qMsgIt != mention.end() ? qMsgIt->second : "Could not load mention but it exists, trust me";
-
-            //         onMention(qUser, qMsg, mention);
-            //     }
-
-            //     log::info("Loaded {} mentions", q.size());
-            // }
-
             auto mentions = co_await evalComments();
 
             if (!mentions.empty()) {
@@ -82,13 +62,6 @@ namespace comments {
                         { "authorIconType", mention["authorStr"]["iconType"] },
                         { "authorGlow", mention["authorStr"]["glow"] }
                     };
-
-                    // Queue mention if the user is playing a level (old, commented out)
-                    // if (PlayLayer::get()) {
-                    //     log::info("Queued mention from @{}, '{}'", mention["authorStr"]["username"], commentDecoded);
-                    //     queue::addToQueue(mentionData);
-                    //     continue;
-                    // }
 
                     onMention(mention["authorStr"]["username"], commentDecoded, mentionData);
                 }
@@ -170,9 +143,5 @@ namespace comments {
             user + " mentioned you",
             msg
         );
-	    // CMUtils::notify(
-	    // 	user + " mentioned you",
-	    // 	msg
-	    // );
     }
 }
