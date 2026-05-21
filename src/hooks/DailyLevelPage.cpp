dc54eadfa7110fdf2b35a384910d2349c226cbb4
xblazegmd <santiagopalaciocolina7@gmail.com>
2026-05-21 17:18:06 -0500

    (try to) fix level hot reloading

--- src/hooks/DailyLevelPage.cpp
+++ src/hooks/DailyLevelPage.cpp
@@ -1,55 +1,36 @@
 #include <Geode/Geode.hpp>
+#include <Geode/Enums.hpp>
 #include <Geode/utils/async.hpp>
 #include <Geode/modify/DailyLevelPage.hpp>
 
 #include <MentionManager.hpp>
-#include <optional>
 
 using namespace geode::prelude;
 
 class $modify(DLPHook, DailyLevelPage) {
-    void levelDownloadFinished(GJGameLevel* level) {
-        DailyLevelPage::levelDownloadFinished(level);
+    void createDailyNode(GJGameLevel* level, bool instant, float delay, bool isNew) {
+        DailyLevelPage::createDailyNode(level, instant, delay, isNew);
+        if (isNew) {
+            switch (m_type) {
+                case GJTimedLevelType::Daily:
+                    if (!Mod::get()->getSettingValue<bool>("daily-lvl")) return;
 
-        int newID = level->m_levelID.value();
-        switch (m_type) {
-            case GJTimedLevelType::Daily:
-                async::spawn(
-                    MentionManager::get()->getDailyID(),
-                    [this, newID](std::optional<int> levelID) {
-                        if (this->isNew(levelID, newID)) {
-                            log::info("New daily level found, reloading...");
-                            async::spawn(MentionManager::get()->fetchDailyID());
-                        }
-                    }
-                );
-                break;
-            case GJTimedLevelType::Weekly:
-                async::spawn(
-                    MentionManager::get()->getWeeklyID(),
-                    [this, newID](std::optional<int> levelID) {
-                        if (this->isNew(levelID, newID)) {
-                            log::info("New weekly demon found, reloading...");
-                            async::spawn(MentionManager::get()->fetchWeeklyID());
-                        }
-                    }
-                );
-                break;
-            case GJTimedLevelType::Event:
-                async::spawn(
-                    MentionManager::get()->getEventID(),
-                    [this, newID](std::optional<int> levelID) {
-                        if (this->isNew(levelID, newID)) {
-                            log::info("New event level found, reloading...");
-                            async::spawn(MentionManager::get()->fetchEventID());
-                        }
-                    }
-                );
-                break;
+                    log::info("New daily level found, reloading...");
+                    async::spawn(MentionManager::get()->fetchDailyID());
+                    break;
+                case GJTimedLevelType::Weekly:
+                    if (!Mod::get()->getSettingValue<bool>("weekly-demon")) return;
+
+                    log::info("New weekly demon found, reloading...");
+                    async::spawn(MentionManager::get()->fetchWeeklyID());
+                    break;
+                case GJTimedLevelType::Event:
+                    if (!Mod::get()->getSettingValue<bool>("event-lvl")) return;
+
+                    log::info("New event level found, reloading...");
+                    async::spawn(MentionManager::get()->fetchEventID());
+                    break;
+            }
         }
-    }
-
-    inline bool isNew(std::optional<int> currentID, int newID) {
-        return currentID && *currentID != newID;
     }
 };
